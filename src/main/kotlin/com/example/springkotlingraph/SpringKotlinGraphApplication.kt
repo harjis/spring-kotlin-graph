1210de3b6534e54fcd28c45d8aa6296c744d6a78
Joonas Harjukallio <harjukallio@gmail.com>
2019-05-20 14:29:33 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/springkotlingraph/SpringKotlinGraphApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlingraph
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinGraphApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinGraphApplication>(*args)
+}
