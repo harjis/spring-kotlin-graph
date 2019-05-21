515bf021662891277ed40168ed216e83e4b30d2e
Joonas Harjukallio <harjukallio@gmail.com>
2019-05-21 16:43:07 +0300

    Maybe goes somewhere

--- src/main/kotlin/com/example/springkotlingraph/app/services/graph/GraphSave.kt
+++ src/main/kotlin/com/example/springkotlingraph/app/services/graph/GraphSave.kt
@@ -3,8 +3,10 @@ package com.example.springkotlingraph.app.services.graph
 import com.example.springkotlingraph.app.entities.Graph
 import com.example.springkotlingraph.app.repositories.GraphRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class GraphSave(private val graphRepository: GraphRepository) {
     fun save(params: GraphSaveParams): Graph {
         return graphRepository.save(params.graph)
--- src/test/kotlin/com/example/springkotlingraph/services/graph/GraphSaveTest.kt
+++ src/test/kotlin/com/example/springkotlingraph/services/graph/GraphSaveTest.kt
@@ -1,22 +1,24 @@
 package com.example.springkotlingraph.services.graph
 
 import com.example.springkotlingraph.app.entities.Graph
+import com.example.springkotlingraph.app.repositories.GraphRepository
 import com.example.springkotlingraph.app.services.graph.GraphSave
 import com.example.springkotlingraph.app.services.graph.GraphSaveParams
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class GraphSaveTest {
+    @Autowired lateinit var graphRepository: GraphRepository
     @Autowired lateinit var graphSave: GraphSave
 
     @Test
     fun savesSimpleStructure() {
         val graph = Graph(name = "Graph 1")
         val params = GraphSaveParams(graph = graph)
-        val savedGraph = graphSave.save(params)
-        println(graph.id)
-        println(savedGraph.id)
+        graphSave.save(params)
+        Assertions.assertThat(graphRepository.findAll().count()).isEqualTo(1);
     }
 }
