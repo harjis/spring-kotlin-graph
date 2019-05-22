5017a6608b758f122b6ee80232b4d02857e0f0e6
Joonas Harjukallio <harjukallio@gmail.com>
2019-05-22 14:49:07 +0300

    Seems to be working still

--- src/main/kotlin/com/example/springkotlingraph/app/services/graph/GraphSave.kt
+++ src/main/kotlin/com/example/springkotlingraph/app/services/graph/GraphSave.kt
@@ -11,31 +11,52 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class GraphSave(private val graphRepository: GraphRepository) {
     fun save(params: GraphSaveParams): Graph {
-        val graph = this.toGraph(params)
+        val graph = if (params.graph.id != null) {
+            this.update(params)
+        } else {
+            this.create(params)
+        }
         return graphRepository.save(graph)
     }
 
-    private fun toGraph(params: GraphSaveParams): Graph {
-        return if (params.graph.id != null) {
-            val graph = graphRepository.findById(params.graph.id).orElseThrow { EntityNotFound(params.graph.id) }
-            val persistedNodeIds: List<Long> = graph.nodes.map { it.id }.filterNotNull()
-            val newNodeIds = params.nodes.map { it.id }.filterNotNull()
-            val toBeDeleted = persistedNodeIds.minus(newNodeIds)
-            params.nodes.filter { it.id != null }.forEach {
-                val persistedNode = graph.nodes.find { node -> node.id == it.id }
-                persistedNode!!.name = it.name
-            }
-            params.nodes.filter { it.id == null }.forEach {
-                Node(name = it.name, graph = graph)
-            }
-            graph.deleteNodes(toBeDeleted)
-            graph.name = params.graph.name
-            graph
-        } else {
-            val graph = Graph(name = params.graph.name)
-            params.nodes.forEach { Node(name = it.name, graph = graph) }
-            graph
+    private fun create(params: GraphSaveParams): Graph {
+        val graph = Graph(name = params.graph.name)
+        params.nodes.forEach { Node(name = it.name, graph = graph) }
+        return graph
+    }
+
+    private fun update(params: GraphSaveParams): Graph {
+        val graph = this.getGraph(params.graph.id)
+        this.updateNodes(params, graph)
+        this.createNodes(params, graph)
+        this.deleteNodes(params, graph)
+        graph.name = params.graph.name
+        return graph
+    }
+
+    private fun updateNodes(params: GraphSaveParams, graph: Graph) {
+        params.nodes.filter { it.id != null }.forEach {
+            val persistedNode = graph.nodes.find { node -> node.id == it.id }
+            persistedNode!!.name = it.name
         }
+    }
+
+    private fun createNodes(params: GraphSaveParams, graph: Graph) {
+        params.nodes.filter { it.id == null }.forEach {
+            Node(name = it.name, graph = graph)
+        }
+    }
+
+    private fun deleteNodes(params: GraphSaveParams, graph: Graph) {
+        val persistedNodeIds: List<Long> = graph.nodes.map { it.id }.filterNotNull()
+        val newNodeIds = params.nodes.map { it.id }.filterNotNull()
+        val toBeDeleted = persistedNodeIds.minus(newNodeIds)
+        graph.deleteNodes(toBeDeleted)
+    }
+
+    private fun getGraph(id: Long?): Graph {
+        if (id == null) throw Exception("Graph can not be updated without primary key")
+        return graphRepository.findById(id).orElseThrow { EntityNotFound(id) }
     }
 }
 
